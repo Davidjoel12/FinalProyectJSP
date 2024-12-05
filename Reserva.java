06f5145e2632b7221d47845442c554ea01d9734d
Nagoru24 <132526125+Nagoru24@users.noreply.github.com>
2024-12-04 21:14:06 -0500

    Create Reserva.java

---
+++ Reserva.java
@@ -0,0 +1,82 @@
+package JAVA;
+
+import java.sql.Date;
+
+public class Reserva {
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String telefono;
+    private String numeroHabitacion;
+    private Date fechaEntrada;
+    private Date fechaSalida;
+
+    // Constructor
+    public Reserva(String nombre, String apellido, String correo, String telefono, 
+                   String numeroHabitacion, Date fechaEntrada, Date fechaSalida) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.numeroHabitacion = numeroHabitacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(String numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
